1c6c6bf34c422d298eaadb9c2c02b7d860bb052b
zhangzhanyi <zhangzhanyi@i-focusing.com>
2018-09-05 09:56:32 +0800

    添加card.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,61 @@
 <template>
-  <div id="app">
-    <el-header>
-      <my-nav :marginlr="marginlr"></my-nav>
-    </el-header>
-    <el-main>
-      <transition name="el-fade-in-linear" mode="out-in">
-        <router-view :style='{"margin-left":this.marginlr,"margin-right":this.marginlr}'></router-view>
-      </transition>
-    </el-main>
-    <el-footer>
-    </el-footer>
-
-  </div>
+    <div id="app">
+        <el-header>
+            <my-nav :marginlr="marginlr"></my-nav>
+        </el-header>
+        <div class="bg">
+        </div>
+        <el-main>
+            <transition name="el-fade-in-linear" mode="out-in">
+                <router-view :style='{"margin-left":this.marginlr,"margin-right":this.marginlr}'></router-view>
+            </transition>
+        </el-main>
+        <el-footer>
+        </el-footer>
+    </div>
 </template>
-
 <script type="text/javascript">
 import nav from 'components/nav/nav.vue'
 
-  export default{
-    data (){
-      return {
-        marginlr:"16%",
-      };
+export default {
+    data() {
+        return {
+            clientSize: {
+              height:'600px',
+              width:'600px',
+            },
+            marginlr: "16%",
+        };
     },
-    components:{
-      "my-nav":nav
-    }
-  }
+    components: {
+        "my-nav": nav
+    },
+    mounted() {
+        this.clientSize.height = `${document.documentElement.clientHeight}px`;
+        this.clientSize.width = `${document.documentElement.clientWidth}px`;
+        const that = this;
+        window.onresize = function() {
+            that.clientSize.height = `${document.documentElement.clientHeight}px`;
+            that.clientSize.width = `${document.documentElement.clientWidth}px`;
+        };
+    },
+}
 </script>
-
 <style>
-main{
-  background-color: #dee3e7;
+.bg {
+    position: relative;
+    max-height: 400px;
+    min-height: 200px;
+    height: 300px;
+    width: 100%;
+    background-image: url('bg.jpg');
+    background-size: cover;
+    background-position: center 50%;
+    background-repeat: no-repeat;
+}
+
+main {
+    background-image: url('bg2.gif');
+    background-color: #eee;
 }
 </style>
---
+++ src/components/card/card.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="card" @click="cardOnClick">
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <div class="time">
+            <slot name="time"></slot>
+        </div>
+        <div class="content">
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        "cardOnClick": function() {
+            console.log("click");
+        }
+    }
+}
+</script>
+<style>
+.card {
+    box-sizing: border-box;
+    width: 98%;
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: 0.1px 0.1px 2px #aaa;
+    padding: 20px;
+    margin: 15px auto;
+    transition: all 0.1s linear;
+}
+
+.card:hover {
+    cursor: pointer;
+    width: 100%;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 2px 2px 6px #aaa;
+    border-radius: 5px;
+    /*font-size: 115%;*/
+    transition: all 0.2s linear;
+}
+
+.title {
+    font-size: 1.17em;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 10px;
+}
+
+.time {
+    font-size: 0.75em;
+    font-weight: 200;
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.content{
+    font-size:0.95em;
+    font-weight: normal;
+}
+</style>
