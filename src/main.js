51b824e9b7c838090eb78913f38d16bd25f81aa7
zhangzhanyi <zhangzhanyi@i-focusing.com>
2018-09-05 09:55:52 +0800

    添加全局css

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import 'normalize.css/normalize.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/icon/iconfont.css'
+import './assets/css/global.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, {
+    size: 'small'
+});
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
