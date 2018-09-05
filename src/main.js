7c683b689bc06507465dbd3ba15a1ec698967605
zhangzhanyi <zhangzhanyi@i-focusing.com>
2018-09-05 10:46:50 +0800

    element改为按需引入

--- src/main.js
+++ src/main.js
@@ -2,16 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'normalize.css/normalize.css'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import './assets/icon/iconfont.css'
 import './assets/css/global.css'
+import {
+    Menu,
+    MenuItem,
+    Header,
+    Main,
+    Footer,
+    Col,
+    Row
+} from 'element-ui';
 
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI, {
-    size: 'small'
-});
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Header);
+Vue.use(Main);
+Vue.use(Footer);
+Vue.use(Col);
+Vue.use(Row);
 
 new Vue({
     router,
