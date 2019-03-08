a0bd07ec6f17c5079d63b45f3835f45b869cc7c8
zhangzhanyi <zhangzhanyi@i-focusing.com>
2019-03-08 11:33:30 +0800

    修复图片不存在时引起的错误

--- src/components/archive/archive.vue
+++ src/components/archive/archive.vue
@@ -3,7 +3,7 @@
         <div>
             <v-hover v-for="item in tags" :key="item.id">
                 <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 10 : 0}`" width="150px" height="150px" style="float: left;" :to="'/archive/'+item.id" class="ma-3">
-                    <v-img class="white--text" :src="item.tagImg.small" style="height: 100%;width:100%;">
+                    <v-img class="white--text" :src="item.tagImg.small || ''" style="height: 100%;width:100%;">
                         <v-container fill-height fluid ma-0 pa-0>
                             <v-layout ma-0 pa-0 fill-height>
                                 <v-flex xs12 flexbox class="white--text text-center vertical-middle" fill-height style="background: rgba(0,0,0,.3);margin-top: auto;margin-bottom: auto;">
